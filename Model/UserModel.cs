95223da0472cef61d5074cf38234f8fff2ffc1f2
ignacio <Ignaciovl.j@gmail.com>
2017-11-05 06:23:08 -0300

    Iniciando en la seguridad del API

---
+++ Model/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiNetCore.Model
+{
+    public class UserModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
